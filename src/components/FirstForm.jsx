18340b7923da7cca28cb3fa6ca5c0b552e7e08c4
hahayz77 <danilo__danilo@hotmail.com>
2023-06-18 10:16:58 -0300

    Form OK

---
+++ src/components/FirstForm.jsx
@@ -0,0 +1,33 @@
+import { Alert, AlertDescription, AlertIcon, AlertTitle } from '@chakra-ui/react';
+import { useForm } from 'react-hook-form';
+import InputMask from 'react-input-mask';
+
+export default function FirstForm() {
+    const { register, handleSubmit, formState: { errors } } = useForm()
+    // console.log(errors)
+    // const [, set] = useState();
+
+    const onSubmit = data => {
+        // console.log(data);
+        if (data.zip_code.length < 9) return console.log("error")
+        console.log(data.zip_code.length);
+
+    }
+
+    return (
+            <form className='relative' onSubmit={handleSubmit(onSubmit)}>
+                <InputMask className='input' mask="99999-999" maskChar="" placeholder="ZIP code" {...register("zip_code", {
+                    required: { value: true, message: "Zip code is Required!" },
+                    minLength: { value: 9, message: "Wrong zip code" }
+                })} />
+                <input className={`btn ${errors?.zip_code?.message ? "btn_error" : ""} `} type="submit" />
+                {errors.zip_code &&
+                    <div className='h-fit my-3 absolute left-0 -bottom-12 w-full'>
+                        <Alert status='error'>
+                            <AlertIcon className='h-4 px-2 animate-pulse text-red-500/80' />
+                            <AlertTitle>{errors?.zip_code?.message}</AlertTitle>
+                        </Alert>
+                    </div>}
+            </form>
+    );
+}
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,17 +1,19 @@
 import { Inter } from 'next/font/google'
 import { useStateContext } from "../context/StateContext";
+import FirstForm from '@/components/FirstForm';
 
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
 
-  const { isAlive } = useStateContext();
+    const { isAlive } = useStateContext();
 
-  return (
-    <main>
-        <h1>Hello World!</h1>
-        <p>{isAlive}</p>
-    </main>
-  )
+    return (
+        <main>
+            <h1>Hello World!</h1>
+            <p>{isAlive}</p>
+            <FirstForm />
+        </main>
+    )
 }
