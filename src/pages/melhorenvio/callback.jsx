3e6473ba40e17e5e01a337c02f999b0b4c7086f1
hahayz77 <danilo__danilo@hotmail.com>
2023-07-29 08:41:32 -0300

    callback page

---
+++ src/pages/melhorenvio/callback.jsx
@@ -0,0 +1,27 @@
+import { useRouter } from 'next/router';
+import { parse } from 'querystring';
+import { serialize } from 'cookie';
+
+
+const MelhorEnvioCallBack = () => {
+    const router = useRouter();
+
+    if (typeof window !== 'undefined') {
+        // Get the 'code' query parameter from the URL
+        const { code } = parse(window.location.search.replace('?', ''));
+
+        // Save the 'code' value as a cookie
+        document.cookie = serialize('code', code, {
+            path: '/',
+            maxAge: 60 * 60, // Cookie expiration time in seconds (e.g., 1 hour)
+        });
+
+        // Redirect to another page or perform any other actions as needed
+        // For example, redirect to a success page:
+        // router.push('/');
+    }
+
+    return null; // or a loading indicator if you want
+}
+
+export default MelhorEnvioCallBack
