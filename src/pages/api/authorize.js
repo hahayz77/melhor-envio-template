af1c857ea2c7539585d3f1cd9b3c8a708dc6974b
hahayz77 <danilo__danilo@hotmail.com>
2023-08-17 15:57:52 -0300

    deleted routes and functions

--- src/pages/api/authorize.js
+++
@@ -1,8 +0,0 @@
-export default async function handler(req, res) {
-  if (req.method === 'GET') {
-    // Perform the redirect
-    // console.log("GET METHOD AUTHORIZE ROUTE")
-    // console.log(process.env.AUTHORIZATION_URL);
-    return res.redirect(307, process.env.AUTHORIZATION_URL);
-  }
-}
